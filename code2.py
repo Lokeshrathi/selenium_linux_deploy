dbb761cf5da3f2e2ee1c90e0fdd52f726e6fd172
Lokesh Rathi <38494676+Lokeshrathi@users.noreply.github.com>
2021-08-18 22:58:39 +0530

    Update code2.py

--- code2.py
+++ code2.py
@@ -4,8 +4,9 @@ from selenium.webdriver.common.keys import Keys
 import time
 firefoxOptions = Options()
 firefoxOptions.add_argument("-headless")
-browser = webdriver.Firefox(executable_path="/usr/bin/geckodriver", options=firefoxOptions)
-browser.get('https://www.linuxhint.com')
+driver = webdriver.Firefox(executable_path="/usr/bin/geckodriver", options=firefoxOptions)
+driver.get("https://www.flipkart.com/search?q=acrylic%20colors&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off%22")
 time.sleep(5)
-print('Title: %s' % browser.title)
-browser.quit()
+list_= driver.find_element_by_xpath('//*[@class = "s1Q9rs"]').text
+print("Here I have come",list_)
+driver.quit()
