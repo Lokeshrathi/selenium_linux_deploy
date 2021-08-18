a42a55bd3e73530eb724dac2f934d6627e2320e8
Lokesh Rathi <38494676+Lokeshrathi@users.noreply.github.com>
2021-08-18 22:44:26 +0530

    Update code2.py

--- code2.py
+++ code2.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
-import pandas as pd 
-import numpy as np
-browser = webdriver.Firefox(executable_path="/usr/bin/geckodriver")
+firefoxOptions = Options()
+firefoxOptions.add_argument("-headless")
+browser = webdriver.Firefox(executable_path="/usr/bin/geckodriver", options=firefoxOptions)
 browser.get('https://www.linuxhint.com')
 print('Title: %s' % browser.title)
 browser.quit()
