24a641be2c514a52bcc9e3a88ec938b052597a29
Lokesh Rathi <38494676+Lokeshrathi@users.noreply.github.com>
2021-08-18 21:04:05 +0530

    Update code.py

--- code.py
+++ code.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 WINDOW_SIZE = "1920,1080"
 chrome_options = Options()
-#chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
