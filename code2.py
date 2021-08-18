558b87a9ef3fd060ea3aa6a59d843c7b32c82cef
Lokesh Rathi <38494676+Lokeshrathi@users.noreply.github.com>
2021-08-18 22:47:08 +0530

    Update code2.py

--- code2.py
+++ code2.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
+import time
 firefoxOptions = Options()
 firefoxOptions.add_argument("-headless")
 browser = webdriver.Firefox(executable_path="/usr/bin/geckodriver", options=firefoxOptions)
 browser.get('https://www.linuxhint.com')
+time.sleep(5)
 print('Title: %s' % browser.title)
 browser.quit()
